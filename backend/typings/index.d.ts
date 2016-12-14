16f44d74c8431ec426917e38d7ba33c1e6d3f0a0
Geass <geass1000@gmail.com>
2016-12-14 03:10:15 +0300

    add 14.12.2016

--- backend/typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/body-parser/index.d.ts" />
-/// <reference path="globals/cookie-parser/index.d.ts" />
-/// <reference path="globals/errorhandler/index.d.ts" />
-/// <reference path="globals/express/index.d.ts" />
-/// <reference path="globals/method-override/index.d.ts" />
-/// <reference path="globals/morgan/index.d.ts" />
