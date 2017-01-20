b44b5ce300ed443e48ca5de619752c40f41d49cc
Geass <geass1000@gmail.com>
2017-01-20 05:37:47 +0300

    add 20.01.2017

--- frontend/app/app.module.ts
+++ frontend/app/app.module.ts
@@ -1,11 +1,18 @@
-import { NgModule }      from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent }  from './app.component';
+import { HomeModule } from './home/home.module';
+import { AppRoutingModule } from './app-routing.module';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports: [
+		BrowserModule,
+		HomeModule,
+		AppRoutingModule
+	],
   declarations: [ AppComponent ],
-  bootstrap:    [ AppComponent ]
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
---
+++ frontend/app/home/home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	moduleId: module.id,
+  selector: 'as-home',
+	templateUrl: 'home.component.html',
+  styleUrls: [ 'home.component.css' ]
+})
+export class HomeComponent  {
+	title = 'Home';
+}
