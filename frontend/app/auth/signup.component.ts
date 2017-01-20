e7327a9e489f34eef69e62c594c07616d39bb1ea
Geass <geass1000@gmail.com>
2017-01-20 21:02:48 +0300

    add 20.01.2017

--- frontend/app/auth/signup.component.ts
+++ frontend/app/auth/signup.component.ts
@@ -20,13 +20,61 @@ export class SignupComponent implements OnInit  {
 
   buildForm(): void {
     this.signupForm = this.fb.group({
-      'login': ['', Validators.required],
+      'login': ['', [
+					Validators.required,
+					Validators.minLength(3),
+					Validators.maxLength(30)
+				]
+			],
 			'email': ['', Validators.required],
-			'password': ['', Validators.required]
+			'password': ['', [
+					Validators.required,
+					Validators.minLength(8),
+					Validators.maxLength(50)
+				]
+			]
     });
+
+		this.signupForm.valueChanges
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged();
   }
+
+	onValueChanged(data?: any) {
+    if (!this.signupForm) { return; }
+    const form = this.signupForm;
+
+    for (const field in form.value) {
+			this.formError[field] = '';
+      const control = form.get(field);
+
+      if (control && control.dirty && !control.valid) {
+        const messages = this.validationMessages[field];
+        for (const key in control.errors) {
+          this.formError[field] += messages[key] + ' ';
+        }
+      }
+    }
+  }
+
 
 	formError = {
 		'login' : ''
 	};
+	validationMessages = {
+    'login': {
+      'required': 'Name is required.',
+      'minlength': 'Name must be at least 3 characters long.',
+      'maxlength': 'Name cannot be more than 30 characters long.'
+    },
+    'email': {
+      'required': 'E-mail is required.'
+    },
+    'password': {
+      'required': 'Password is required.',
+			'minlength': 'Password must be at least 8 characters long.',
+      'maxlength': 'Password cannot be more than 50 characters long.'
+    }
+  };
 }
