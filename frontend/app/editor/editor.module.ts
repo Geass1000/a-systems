bd0a6ecdfc795220d622f6509201a1b8f7b33751
Geass <geass1000@gmail.com>
2017-03-24 17:21:53 +0300

    add 24.03.2017

--- frontend/app/editor/editor.module.ts
+++ frontend/app/editor/editor.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 /* Routing Module */
 import { EditorRoutingModule } from './editor-routing.module';
 
 /* App Feature - Component */
 import { EditorComponent } from './editor.component';
+import { WorkstateComponent } from './workstate/workstate.component';
 import { RoomComponent } from './room/room.component';
 
 /* App Feature - Service */
 import { AuthService } from '../auth/auth.service';
 
 @NgModule({
-  imports:      [ CommonModule, EditorRoutingModule ],
+  imports: [
+		CommonModule,
+		FormsModule,
+		EditorRoutingModule
+	],
   declarations: [
 		EditorComponent,
+		WorkstateComponent,
 		RoomComponent
 	],
   exports:      [ EditorComponent ],
