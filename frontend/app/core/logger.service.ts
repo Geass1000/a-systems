6db735094d4b5478c59ddd631af83b807c325d2b
Geass <geass1000@gmail.com>
2017-05-24 05:05:28 +0300

    add 24.05.2017

--- frontend/app/core/logger.service.ts
+++ frontend/app/core/logger.service.ts
@@ -1,6 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Optional } from '@angular/core';
 
-enum Level {
+export enum Level {
 	OFF = 0,
 	ERROR = 1,
 	WARN = 2,
@@ -9,46 +9,120 @@ enum Level {
 	LOG = 5
 };
 
+
+export class Options {
+	level : Level;
+}
+
+const DEFAULT_OPTIONS : Options = {
+	level : Level.INFO
+};
+
 @Injectable()
 export class LoggerService {
 	private level : Level = Level.LOG;
 	private availableLevel : string[] = ['error', 'warn', 'info', 'debug', 'log'];
-	constructor () {
+
+	constructor (@Optional() options ?: Options) {
+		let opts : Options = <Options>Object.assign({}, DEFAULT_OPTIONS, options);
+		this.level = opts.level;
 	}
 
-	error (...restOfMessage : any[]) {
+	/**
+	 * Переопределения console.error.
+	 *
+	 * @method
+	 *
+	 * @param {Array<any>} restOfMessage - выводимые сообщения
+	 * @return {void}
+	 */
+	error (...restOfMessage : Array<any>) : void {
 		if (this.isEnable(Level.ERROR)) {
 			console.error.apply(console, restOfMessage);
 		}
 	}
-	warn (...restOfMessage : any[]) {
+
+	/**
+	 * Переопределения console.warn.
+	 *
+	 * @method
+	 *
+	 * @param {Array<any>} restOfMessage - выводимые сообщения
+	 * @return {void}
+	 */
+	warn (...restOfMessage : Array<any>) : void {
 		if (this.isEnable(Level.WARN)) {
 			console.warn.apply(console, restOfMessage);
 		}
 	}
-	info (...restOfMessage : any[]) {
+
+	/**
+	 * Переопределения console.info.
+	 *
+	 * @method
+	 *
+	 * @param {Array<any>} restOfMessage - выводимые сообщения
+	 * @return {void}
+	 */
+	info (...restOfMessage : Array<any>) : void {
 		if (this.isEnable(Level.INFO)) {
 			console.info.apply(console, restOfMessage);
 		}
 	}
-	debug (...restOfMessage : any[]) {
+
+	/**
+	 * Переопределения console.debug.
+	 *
+	 * @method
+	 *
+	 * @param {Array<any>} restOfMessage - выводимые сообщения
+	 * @return {void}
+	 */
+	debug (...restOfMessage : Array<any>) : void {
 		if (this.isEnable(Level.DEBUG)) {
 			(console.debug || console.log).apply(console, restOfMessage);
 		}
 	}
-	log (...restOfMessage : any[]) {
+
+	/**
+	 * Переопределения console.log.
+	 *
+	 * @method
+	 *
+	 * @param {Array<any>} restOfMessage - выводимые сообщения
+	 * @return {void}
+	 */
+	log (...restOfMessage : Array<any>) : void {
 		if (this.isEnable(Level.LOG)) {
 			console.log.apply(console, restOfMessage);
 		}
 	}
-	print (logLevel : string, ...restOfMessage : any[]) {
+
+	/**
+	 * Переопределение console.print.
+	 *
+	 * @method
+	 *
+	 * @param {string} logLevel - название уровня логгирования
+	 * @param {Array<any>} restOfMessage - выводимые сообщения
+	 * @return {void}
+	 */
+	print (logLevel : string, ...restOfMessage : Array<any>) : void {
 		if (this.availableLevel.indexOf(logLevel) === -1) {
 			throw 'Level not exist';
 		}
 		this[logLevel].apply(this, restOfMessage);
 	}
 
-	private isEnable (level : Level) {
+	/**
+	 * isEnable - выполняет проверку текущего уровня логгирования.
+	 *
+	 * @method
+	 *
+	 * @param {Level} level - уровень метода
+	 * @return {boolean}
+	 */
+	private isEnable (level : Level) : boolean {
 		return this.level >= level;
 	}
 
